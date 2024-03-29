a7bf48f4d9938ed58c8f02f7e346f5cc4251c5d2
Navin3d <smnavin65@gmail.com>
2024-03-29 12:41:17 +0530

    changed packages and minor refactors

--- src/main/java/gmc/learning/reactive/management/project/models/MailModel.java
+++
@@ -1,7 +0,0 @@
-package gmc.learning.reactive.management.project.models;
-
-import java.io.Serializable;
-
-public record MailModel(String to, String subject, String body) implements Serializable {
-
-}
